295b1fc516cfcc5604605f2542b0765cc2971915
tess3rac7 <tess3rac7@protonmail.com>
2023-06-06 13:35:27 -0400

    Update UniV2Mixin to use ISwapErrors and include router in mapping

--- src/mixins/UniV2Mixin.sol
+++ src/mixins/UniV2Mixin.sol
@@ -4,36 +4,55 @@ pragma solidity ^0.8.0;
 
 import "oz/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/IUniswapV2Router02.sol";
+import "../interfaces/ISwapErrors.sol";
 import "../libraries/Babylonian.sol";
 
-abstract contract UniV2Mixin {
+abstract contract UniV2Mixin is ISwapErrors {
     using SafeERC20 for IERC20;
 
-    /// @dev tokenA => (tokenB => path): returns best path to swap
-    ///         tokenA to tokenB
-    mapping(address => mapping(address => address[])) public uniV2SwapPaths;
+    /// @dev tokenA => (tokenB => (router => path)): returns best path to swap
+    ///         tokenA to tokenB for the given router (protocol)
+    mapping(address => mapping(address => mapping(address => address[]))) public uniV2SwapPaths;
 
     /// @dev Helper function to swap {_from} to {_to} given an {_amount}.
-    function _swapUniV2(address _from, address _to, uint256 _amount, uint256 _minAmountOut, address _router) internal {
+    function _swapUniV2(address _from, address _to, uint256 _amount, uint256 _minAmountOut, address _router)
+        internal
+        returns (uint256 amountOut)
+    {
         if (_from == _to || _amount == 0) {
-            return;
+            return 0;
         }
-        address[] storage path = uniV2SwapPaths[_from][_to];
+        address[] storage path = uniV2SwapPaths[_from][_to][_router];
         require(path.length >= 2, "Missing path for swap");
 
-        IUniswapV2Router02 router = IUniswapV2Router02(_router);
-        try router.getAmountsOut(_amount, path) returns (uint256[] memory amounts) {
-            // return if final output is 0 (don't fail harvest)
-            if (amounts[path.length - 1] == 0) return;
-        } catch {
-            // return if intermediate output is 0 (don't fail harvest)
-            return;
+        uint256 predictedOutput = _getAmountOut(_amount, path, _router);
+        if (predictedOutput == 0) {
+            emit GetAmountsOutFailed(_router, _amount, _from, _to);
+            return 0;
         }
 
+        uint256 toBalBefore = IERC20(_to).balanceOf(address(this));
         IERC20(_from).safeIncreaseAllowance(_router, _amount);
-        router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        try IUniswapV2Router02(_router).swapExactTokensForTokensSupportingFeeOnTransferTokens(
             _amount, _minAmountOut, path, address(this), block.timestamp
-        );
+        ) {
+            amountOut = IERC20(_to).balanceOf(address(this)) - toBalBefore;
+        } catch {
+            emit SwapFailed(_router, _amount, _minAmountOut, _from, _to);
+        }
+    }
+
+    /// @dev Helper function to get the predicted output of a swap given an {_amount}, {_path} and {_router}
+    function _getAmountOut(uint256 _amount, address[] storage _path, address _router)
+        private
+        view
+        returns (uint256 amountOut)
+    {
+        try IUniswapV2Router02(_router).getAmountsOut(_amount, _path) returns (uint256[] memory amounts) {
+            amountOut = amounts[_path.length - 1];
+        } catch {
+            amountOut = 0;
+        }
     }
 
     /**
@@ -65,13 +84,17 @@ abstract contract UniV2Mixin {
     }
 
     /// @dev Update {SwapPath} for a specified pair of tokens.
-    function _updateUniV2SwapPath(address _tokenIn, address _tokenOut, address[] calldata _path) internal {
+    function _updateUniV2SwapPath(address _tokenIn, address _tokenOut, address _router, address[] calldata _path)
+        internal
+    {
         require(
             _tokenIn != _tokenOut && _path.length >= 2 && _path[0] == _tokenIn && _path[_path.length - 1] == _tokenOut
         );
-        uniV2SwapPaths[_tokenIn][_tokenOut] = _path;
+        uniV2SwapPaths[_tokenIn][_tokenOut][_router] = _path;
     }
 
     // Be sure to permission this in implementation
-    function updateUniV2SwapPath(address _tokenIn, address _tokenOut, address[] calldata _path) external virtual;
+    function updateUniV2SwapPath(address _tokenIn, address _tokenOut, address _router, address[] calldata _path)
+        external
+        virtual;
 }
