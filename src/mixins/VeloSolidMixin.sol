3ac8407f32e9f27d283c495bbb3a696d94f5270f
tess3rac7 <tess3rac7@protonmail.com>
2023-06-06 17:33:46 -0400

    Update VeloSolidMixin to use router in mapping

--- src/mixins/VeloSolidMixin.sol
+++ src/mixins/VeloSolidMixin.sol
@@ -11,25 +11,28 @@ import "../libraries/Babylonian.sol";
 abstract contract VeloSolidMixin is ISwapErrors {
     using SafeERC20 for IERC20;
 
-    /// @dev tokenA => (tokenB => veloSwapPath config): returns best path to swap
-    ///         tokenA to tokenB
-    mapping(address => mapping(address => address[])) public veloSwapPaths;
+    /// @dev tokenA => (tokenB => (router => path): returns best path to swap
+    ///         tokenA to tokenB for the given router (protocol)
+    mapping(address => mapping(address => mapping(address => address[]))) public veloSwapPaths;
 
     /// @dev Helper function to swap {_from} to {_to} given an {_amount}.
-    function _swapVelo(address _from, address _to, uint256 _amount, uint256 _minAmountOut, address _router) internal {
+    function _swapVelo(address _from, address _to, uint256 _amount, uint256 _minAmountOut, address _router)
+        internal
+        returns (uint256 amountOut)
+    {
         if (_from == _to || _amount == 0) {
-            return;
+            return 0;
         }
-        address[] storage path = veloSwapPaths[_from][_to];
+        address[] storage path = veloSwapPaths[_from][_to][_router];
         require(path.length >= 2, "Missing path for swap");
 
-        uint256 output;
-        bool useStable;
         IVeloRouter router = IVeloRouter(_router);
         IVeloRouter.route[] memory routes = new IVeloRouter.route[](path.length - 1);
         uint256 prevRouteOutput = _amount;
 
         for (uint256 i = 0; i < routes.length; i++) {
+            uint256 output;
+            bool useStable;
             try router.getAmountOut(prevRouteOutput, path[i], path[i + 1]) returns (
                 uint256 tmpOutput, bool tmpUseStable
             ) {
@@ -38,19 +41,20 @@ abstract contract VeloSolidMixin is ISwapErrors {
             } catch {}
 
             if (output == 0) {
-                emit GetAmountsOutFailed(_router, prevRouteOutput, path[i], path[i + 1]);
-                return;
+                emit GetAmountsOutFailed(_router, _amount, _from, _to);
+                return 0;
             }
 
             routes[i] = IVeloRouter.route({from: path[i], to: path[i + 1], stable: useStable});
             prevRouteOutput = output;
-            output = 0;
         }
 
+        uint256 toBalBefore = IERC20(_to).balanceOf(address(this));
         IERC20(_from).safeIncreaseAllowance(_router, _amount);
-        try router.swapExactTokensForTokens(_amount, _minAmountOut, routes, address(this), block.timestamp) {}
-        catch {
-            emit SwapFailed(_router, _amount, _minAmountOut, routes[0].from, routes[routes.length - 1].to);
+        try router.swapExactTokensForTokens(_amount, _minAmountOut, routes, address(this), block.timestamp) {
+            amountOut = IERC20(_to).balanceOf(address(this)) - toBalBefore;
+        } catch {
+            emit SwapFailed(_router, _amount, _minAmountOut, _from, _to);
         }
     }
 
@@ -78,13 +82,17 @@ abstract contract VeloSolidMixin is ISwapErrors {
     }
 
     /// @dev Update {SwapPath} for a specified pair of tokens.
-    function _updateVeloSwapPath(address _tokenIn, address _tokenOut, address[] calldata _path) internal {
+    function _updateVeloSwapPath(address _tokenIn, address _tokenOut, address _router, address[] calldata _path)
+        internal
+    {
         require(
             _tokenIn != _tokenOut && _path.length >= 2 && _path[0] == _tokenIn && _path[_path.length - 1] == _tokenOut
         );
-        veloSwapPaths[_tokenIn][_tokenOut] = _path;
+        veloSwapPaths[_tokenIn][_tokenOut][_router] = _path;
     }
 
     // Be sure to permission this in implementation
-    function updateVeloSwapPath(address _tokenIn, address _tokenOut, address[] calldata _path) external virtual;
+    function updateVeloSwapPath(address _tokenIn, address _tokenOut, address _router, address[] calldata _path)
+        external
+        virtual;
 }
