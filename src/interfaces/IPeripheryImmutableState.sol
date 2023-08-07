448bbb7e1e74f1259610b8a7672ea860916d4e0c
degenicus <degenicus@bytemasons.com>
2023-08-07 16:10:56 +0200

    Simplify interfaces to used functions

--- src/interfaces/IPeripheryImmutableState.sol
+++ src/interfaces/IPeripheryImmutableState.sol
@@ -6,7 +6,4 @@ pragma solidity >=0.5.0;
 interface IPeripheryImmutableState {
     /// @return Returns the address of the Uniswap V3 factory
     function factory() external view returns (address);
-
-    /// @return Returns the address of WETH9
-    function WETH9() external view returns (address);
 }
