b25c8cc81fda97d2037438cd46ccf0d966a1a2a7
Oleg <martelskiy@gmail.com>
2024-04-29 11:00:03 +0200

    Make 'fetchManagementFeeBPS' function view

--- src/interfaces/IFeeController.sol
+++ src/interfaces/IFeeController.sol
@@ -3,6 +3,6 @@
 pragma solidity ^0.8.0;
 
 interface IFeeController {
-    function fetchManagementFeeBPS() external returns (uint16);
+    function fetchManagementFeeBPS() external view returns (uint16);
     function updateManagementFeeBPS(uint16 _feeBPS) external;
 }
