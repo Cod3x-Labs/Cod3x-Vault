d03a2426eef19e811cbe70eebbb3c7b0d64f71de
Zokunei <zokunei@bytemasons.com>
2024-08-23 14:28:36 -0400

    tokenB :^)

--- src/interfaces/IVeloV1AndV2Factory.sol
+++ src/interfaces/IVeloV1AndV2Factory.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 interface IVeloV1AndV2Factory {
     function isPool(address pool) external view returns (bool);
-    function getPool(address tokenA, address token, bool stable) external view returns (address);
+    function getPool(address tokenA, address tokenB, bool stable) external view returns (address);
 }
