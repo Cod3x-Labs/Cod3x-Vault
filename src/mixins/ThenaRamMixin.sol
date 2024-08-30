7d73f52f8f690fc8136dcee6a8ecca19120a52dc
Zokunei <zokunei@bytemasons.com>
2024-08-30 12:22:22 -0400

    don't use fee on transfer function (to support Tokan + NURI)

--- src/mixins/ThenaRamMixin.sol
+++ src/mixins/ThenaRamMixin.sol
@@ -50,7 +50,7 @@ abstract contract ThenaRamMixin is ISwapErrors {
         IERC20(_from).safeIncreaseAllowance(_router, _amount);
         // Based on configurable param catch fails or just revert
         if (_tryCatchActive != false) {
-            try router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            try router.swapExactTokensForTokens(
                 _amount, _minAmountOut, path, address(this), _deadline
             ) {
                 amountOut = IERC20(_to).balanceOf(address(this)) - toBalBefore;
@@ -59,7 +59,7 @@ abstract contract ThenaRamMixin is ISwapErrors {
                 emit SwapFailed(_router, _amount, _minAmountOut, _from, _to);
             }
         } else {
-            router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            router.swapExactTokensForTokens(
                 _amount, _minAmountOut, path, address(this), _deadline
             );
             amountOut = IERC20(_to).balanceOf(address(this)) - toBalBefore;
